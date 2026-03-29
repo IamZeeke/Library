a34e482c635772c5c54f0a2565ae3c453d996dd4
IamZeeke <99756004+IamZeeke@users.noreply.github.com>
2026-03-29 09:29:31 -0700

    Create UltimateLibraryPlugin.java

---
+++ UltimateLibraryPlugin.java
@@ -0,0 +1,46 @@
+package com.xai.ultimatelibrary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UltimateLibraryPlugin extends JavaPlugin {
+
+    private static UltimateLibraryPlugin instance;
+    private StructureBuilder structureBuilder;
+    private AltarManager altarManager;
+    private BossManager bossManager;
+    private WeaponManager weaponManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        structureBuilder = new StructureBuilder(this);
+        altarManager = new AltarManager(this);
+        bossManager = new BossManager(this);
+        weaponManager = new WeaponManager(this);
+
+        getLogger().info("§6[UltimateLibrary] §aFull Library Dungeon plugin loaded!");
+
+        getCommand("buildlibrary").setExecutor(new CommandHandler(this));
+        getCommand("regeneratelibrary").setExecutor(new CommandHandler(this));
+
+        getServer().getPluginManager().registerEvents(altarManager, this);
+        getServer().getPluginManager().registerEvents(bossManager, this);
+        getServer().getPluginManager().registerEvents(weaponManager, this);
+    }
+
+    public static UltimateLibraryPlugin getInstance() {
+        return instance;
+    }
+
+    public StructureBuilder getStructureBuilder() {
+        return structureBuilder;
+    }
+
+    public AltarManager getAltarManager() {
+        return altarManager;
+    }
+
+    public BossManager getBossManager() {
+        return bossManager;
+    }
+}
